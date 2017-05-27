7783c80b3b2789f5ea59804a1e40a1c6414f581d
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-05-27 23:59:55 +0200

    Initial commit

---
+++ src/Controller/ApidaeController.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace Drupal\apidae_drupal_module\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Language\Language;
+use Drupal\node\Entity\Node;
+use Sitra\ApiClient\Exception\SitraException;
+use Sitra\ApiClient\SitraServiceClient;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApidaeController extends ControllerBase {
+
+  public function import() {
+    \Drupal::logger('Apidae')->info('Importing Apidae data');
+
+    $apidaeConfig = $this->config('system.apidae');
+    $apiUrl = $apidaeConfig->get('api.url');
+    $apiKey = $apidaeConfig->get('api.key');
+    $apiProject = $apidaeConfig->get('api.project');
+    $selections =  $apidaeConfig->get('data.selections');
+    $objectsTypes =  array_filter($apidaeConfig->get('data.types'));
+
+    \Drupal::logger('Apidae')->info('config ok');
+
+    $selections_ids = array_map('intval', explode(',', (string)$selections));
+    \Drupal::logger('Apidae')->info('selections ids ok');
+
+    $client = $this->createClient($apiUrl, $apiKey, $apiProject);
+
+    $objectsCount = 0;
+
+    \Drupal::logger('Apidae')->info('Client created');
+
+    if($client) {
+      foreach ($objectsTypes as $objType) {
+        \Drupal::logger('Apidae')->info('Importing data for type : '.$objType);
+        try {
+          $results = $client->searchObject([
+            'query' => [
+              "apiKey" => $apiKey,
+              "projetId" => $apiProject,
+              "selectionIds" => $selections_ids,
+              "criteresQuery" => "type:" . $objType,
+              "responseFields" => ["id", "nom", "illustrations", "multimedias", "informations", "presentation",
+                "localisation", "@informationsObjetTouristique", "ouverture.periodeEnClair",
+                "ouverture.periodesOuvertures", "descriptionTarif.tarifsEnClair.LibelleFr", "contacts"]
+            ]
+          ]);
+
+          if (isset($results['objetsTouristiques'])) {
+            $i = count($results['objetsTouristiques']);
+            $objectsCount += $i;
+            foreach ($results['objetsTouristiques'] as $result) {
+              \Drupal::logger('Apidae result')->info('test');
+              $this->createNode($result);
+            }
+          }
+        } catch(SitraException $e) {
+          \Drupal::logger('Apidae module')->error('An error occurred during the retrieval of Apidae data. Please make sure that all configuration values have been properly set.');
+          \Drupal::logger('Apidae module')->error($e->getMessage());
+        }
+      }
+      \Drupal::logger('Apidae module')->info('%d objects updated/created successfully', array('%d' => $objectsCount));
+    }
+
+    return new Response('', 204);
+  }
+
+  private function createClient($url, $key, $id) {
+    try {
+      \Drupal::logger('Apidae')->info('before client');
+      $client = new SitraServiceClient([
+        'baseUri' => $url,
+        'apiKey' => $key,
+        'projectId' => $id,
+        'count' => 100,
+      ]);
+      \Drupal::logger('Apidae')->info('after client');
+
+      return $client;
+    } catch (SitraException $e) {
+      \Drupal::logger('Apidae module')->error('An error occurred during the connection setup with Apidae. Please make sure that all configuration values have been properly set.');
+      \Drupal::logger('Apidae module')->error($e->getMessage());
+      return null;
+    }
+  }
+
+  private function checkNodeExists($id) {
+    $result = null;
+    $nodes = node_load_multiple(array(), array('type' => 'apidae_object'));
+    foreach ($nodes as $value) {
+      if (isset($value->ao_id) && ($value->ao_id['und'][0]['value'] == $id)) {
+        $result = $value->nid;
+      }
+    }
+    return $result;
+  }
+
+  private function createApidaeObject() {
+    $node = Node::create([
+      'type' => 'apidae_object',
+      'language' => 'fr',
+      'status' => 1,
+      'sticky' => 0,
+      'promote' => 0,
+      'comment' => 0,
+      'created' => time(),
+      'updated' => time()
+    ]);
+    return $node;
+  }
+
+  private function createNode($content)
+  {
+    $refreshMode = $this->config('system.apidae')->get('cron.type');
+    $contentId = $content['id'];
+    $nid = $this->checkNodeExists($contentId);
+    $lang = Language::LANGCODE_NOT_SPECIFIED;
+
+    if (is_null($nid) || (!is_null($nid) && $refreshMode == 'new_content_and_updates')) {
+
+      //if node doesn't exist or if it exists and could not load it
+      if (is_null($nid) || (!($node = node_load($nid)))) {
+        $node = $this->createApidaeObject();
+      }
+
+      //todo : check the field value to see if it was updated on apidae, and update the field only if it was
+      //General fields
+      if (isset($content['presentation']['descriptifDetaille']['libelleFr'])) {
+        $complaint_body = $content['presentation']['descriptifDetaille']['libelleFr'];
+      } elseif (isset($content['presentation']['descriptifCourt']['libelleFr'])) {
+        $complaint_body = $content['presentation']['descriptifCourt']['libelleFr'];
+      } else {
+        $complaint_body = '';
+      }
+
+      $node->set('ao_id', $contentId);
+      $node->setTitle($content['nom']['libelleFr']);
+      $node->set('body', array(
+        'value' => nl2br($complaint_body),
+        'format' => 'full_html',
+        'summary' => isset($content['presentation']['descriptifCourt']['libelleFr']) ? $content['presentation']['descriptifCourt']['libelleFr'] : text_summary(nl2br($complaint_body))
+      ));
+
+//      if (isset($content['presentation']['descriptifCourt']['libelleFr'])) {
+//        $node->body->summary = $content['presentation']['descriptifCourt']['libelleFr'];
+//      } else {
+//        $node->body->summary = text_summary(nl2br($complaint_body));
+//      }
+//      $node->body->format = 'full_html';
+
+      // todo : setup yaml-based taxonomy (see https://www.metaltoad.com/blog/drupal-8-migrations-part-3-migrating-taxonomies-drupal-7)
+//      $type = $content['type'];
+//      $tid = _get_tid_from_type($type);
+//      $node->ao_type['und'][0] = array('tid' => $tid);
+
+      // location data
+      if (isset($content['localisation']['adresse']['adresse1'])) {
+        $node->set('ao_address1', nl2br($content['localisation']['adresse']['adresse1']));
+      }
+      if (isset($content['localisation']['adresse']['adresse2'])) {
+        $node->set('ao_address2', nl2br($content['localisation']['adresse']['adresse2']));
+      }
+      if (isset($content['localisation']['adresse']['adresse3'])) {
+        $node->set('ao_address3', nl2br($content['localisation']['adresse']['adresse3']));
+      }
+      if (isset($content['localisation']['adresse']['codePostal'])) {
+        $node->set('ao_postal_code', $content['localisation']['adresse']['codePostal']);
+      }
+      if (isset($content['localisation']['adresse']['commune']['nom'])) {
+        $node->set('ao_town', nl2br($content['localisation']['adresse']['commune']['nom']));
+      }
+      if (isset($content['localisation']['geolocalisation']['geoJson']['coordinates']['0'])) {
+        $node->set('ao_latitude', $content['localisation']['geolocalisation']['geoJson']['coordinates']['1']);
+      }
+      if (isset($content['localisation']['geolocalisation']['geoJson']['coordinates']['1'])) {
+        $node->set('ao_longitude', $content['localisation']['geolocalisation']['geoJson']['coordinates']['0']);
+      }
+
+      // contact info
+      if (isset($content['informations']['moyensCommunication'])) {
+        foreach ($content['informations']['moyensCommunication'] as $key => $value) {
+          switch ($value['type']['id']) {
+            case '201' :
+              $node->set('ao_telephone', $value['coordonnees']['fr']);
+              break;
+            case '204' :
+              $node->set('ao_email', $value['coordonnees']['fr']);
+              break;
+            case '205' :
+              $node->set('ao_website', $value['coordonnees']['fr']);
+              break;
+            case '207' :
+              $node->set('ao_facebook', $value['coordonnees']['fr']);
+              break;
+            default :
+              break;
+          }
+        }
+      }
+
+      if (isset($content['presentation']['descriptifCourt']['libelleFr'])) {
+        $node->set('ao_short_desc', nl2br($content['presentation']['descriptifCourt']['libelleFr']));
+      }
+
+      // first picture fields
+      if (isset($content['illustrations'][0]['traductionFichiers'][0]['url'])) {
+        $node->set('ao_pic1_large', $content['illustrations'][0]['traductionFichiers'][0]['url']);
+      }
+      if (isset($content['illustrations'][0]['traductionFichiers'][0]['urlDiaporama'])) {
+        $node->set('ao_pic1_medium', $content['illustrations'][0]['traductionFichiers'][0]['urlDiaporama']);
+      }
+      if (isset($content['illustrations'][0]['nom']['libelleFr'])) {
+        $node->set('ao_pic1_title', $content['illustrations'][0]['nom']['libelleFr']);
+      }
+      if (isset($content['illustrations'][0]['copyright']['libelleFr'])) {
+        $node->set('ao_pic1_credits', $content['illustrations'][0]['copyright']['libelleFr']);
+      }
+
+      if (isset($content['multimedias'])) {
+        foreach ($content['multimedias'] as $key => $value) {
+          switch ($value['type']) {
+            case 'VIDEO' :
+              $node->set('ao_video_url', $value['traductionFichiers']['0']['url']);
+              if (isset($value['nom']['libelleFr'])) {
+                $node->set('ao_video_title', $value['nom']['libelleFr']);
+              }
+              break;
+            case 'DOCUMENT' :
+              if (strpos($value['traductionFichiers'][0]['url'], 'pdf')) {
+                $node->set('ao_pdf_url', $value['traductionFichiers'][0]['url']);
+                if (isset($value['nom']['libelleFr'])) {
+                  $node->set('ao_pdf_title', $value['nom']['libelleFr']);
+                }
+                break;
+              }
+          }
+        }
+      }
+
+      if (isset($content['ouverture']['periodeEnClair']['libelleFr'])) {
+        $node->set('ao_openings', nl2br($content['ouverture']['periodeEnClair']['libelleFr']));
+      }
+
+      if (isset($content['descriptionTarif']['tarifsEnClair']['libelleFr'])) {
+        $node->set('ao_rates', nl2br($content['descriptionTarif']['tarifsEnClair']['libelleFr']));
+      }
+
+      $node->save();
+    }
+  }
+}
---
+++ src/Form/ApidaeSettingsForm.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Drupal\apidae_drupal_module\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class ApidaeSettingsForm extends ConfigFormBase {
+
+  protected function getEditableConfigNames() {
+    return ['system.apidae'];
+  }
+
+  public function getFormId() {
+    return 'system_apidae_settings';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $apidaeConfig = \Drupal::config('system.apidae');
+    // apidae_api and selections were identifiants / apidae_cron was maj
+    $form['apidae_api'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Informations de connexion'),
+      '#description' => t('Ces informations sont disponibles dans votre fiche projet sur https://base.apidae-tourisme.com')
+    );
+    $form['apidae_api']['url'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Adresse web (URL)'),
+      '#required' => TRUE,
+      '#default_value' => $apidaeConfig->get('api.url')
+    );
+    $form['apidae_api']['key'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Clé d\'API'),
+      '#required' => TRUE,
+      '#default_value' => $apidaeConfig->get('api.key')
+    );
+    $form['apidae_api']['project'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Identifiant'),
+      '#required' => TRUE,
+      '#default_value' => $apidaeConfig->get('api.project')
+    );
+    $form['apidae_cron'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Mise à jour automatique des données'),
+      '#description' => t('Activez ou non l\'actualisation automatique des données via le Cron de drupal')
+    );
+    $form['apidae_cron']['active'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Activer la mise à jour automatique des données'),
+      '#default_value' => $apidaeConfig->get('cron.active')
+    );
+    $options = array(
+      'new_content_and_updates' => t('Activer la récupération de nouveaux contenus ainsi que la mise à jour des données déjà récupérées <strong>(Ceci écrasera les éventuels changements que vous avez apporté au contenu.)</strong>'),
+      'new_content_only' => t('Activer <strong>Uniquement</strong> la récupération de nouveaux contenus.')
+    );
+    $form['apidae_cron']['type'] = array(
+      '#type' => 'radios',
+      '#title' => t('Quel type de mise à jour souhaitez-vous ?'),
+      '#options' => $options,
+      '#description' => t('Valable uniquement si les mises à jour sont activées.'),
+      '#default_value' => $apidaeConfig->get('cron.type')
+    );
+    $form['apidae_cron']['frequency'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Fréquence (en jours) des mises à jour'),
+      '#description' => t('Requis si les mises à jours automatiques sont activées. Valeur en nombre de jours. Pour une MAJ toutes les semaines, entrez 7'),
+      '#default_value' => $apidaeConfig->get('cron.frequency')
+    );
+    $form['apidae_data'] = array(
+      '#type' => 'fieldset',
+      '#title' => t('Données Apidae à importer'),
+      '#description' => t('Sélectionnez les critères des objets touristiques à récupérer')
+    );
+    $form['apidae_data']['selections'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Sélections Apidae pré-calculées'),
+      '#description' => t('Si vous avez plusieurs sélections, séparez-les par une virgule'),
+      '#required' => FALSE,
+      '#default_value' => $apidaeConfig->get('data.selections')
+    );
+    $form['apidae_data']['types'] = array(
+      '#type' => 'checkboxes',
+      '#title' => t('Sélection des types d\'objets touristiques : '),
+      '#options' => array('ACTIVITE' => t('Activité'), 'COMMERCE_ET_SERVICE' => t('Commerce et service'), 'DEGUSTATION' => t('Producteur'),
+        'DOMAINE_SKIABLE' => t('Domaine skiable'), 'EQUIPEMENT' => t('Equipement'), 'FETE_ET_MANIFESTATION' => t('Fête et manifestation'),
+        'HEBERGEMENT_COLLECTIF' => t('Hébergement collectif'), 'HEBERGEMENT_LOCATIF' => t('Hébergement locatif'), 'HOTELLERIE' => t('Hôtellerie'),
+        'HOTELLERIE_PLEIN_AIR' => t('Hôtellerie de plein air'), 'PATRIMOINE_CULTUREL' => t('Patrimoine culturel'),
+        'PATRIMOINE_NATUREL' => t('Patrimoine naturel'), 'RESTAURATION' => t('Restauration'), 'SEJOUR_PACKAGE' => t('Séjour packagé'),
+        'TERRITOIRE' => t('Territoire')),
+      '#default_value' => array('ACTIVITE', 'COMMERCE_ET_SERVICE', 'DEGUSTATION', 'DOMAINE_SKIABLE',
+        'EQUIPEMENT', 'FETE_ET_MANIFESTATION', 'HEBERGEMENT_COLLECTIF', 'HEBERGEMENT_LOCATIF', 'HOTELLERIE', 'HOTELLERIE_PLEIN_AIR', 'PATRIMOINE_CULTUREL',
+        'PATRIMOINE_NATUREL', 'RESTAURATION', 'SEJOUR_PACKAGE', 'TERRITOIRE')
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('system.apidae')
+      ->set('api.url', $form_state->getValue('url'))
+      ->set('api.key', $form_state->getValue('key'))
+      ->set('api.project', $form_state->getValue('project'))
+      ->set('cron.active', $form_state->getValue('active'))
+      ->set('cron.type', $form_state->getValue('type'))
+      ->set('cron.frequency', $form_state->getValue('frequency'))
+      ->set('data.selections', $form_state->getValue('selections'))
+      ->set('data.types', $form_state->getValue('types'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+}
