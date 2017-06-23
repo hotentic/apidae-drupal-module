b507138000019a642db3b56d4e777e35c87ab7f1
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-06-23 23:42:52 +0200

    Increased config form fields size

--- src/Form/ApidaeSettingsForm.php
+++ src/Form/ApidaeSettingsForm.php
@@ -25,7 +25,7 @@ class ApidaeSettingsForm extends ConfigFormBase {
     );
     $form['apidae_api']['url'] = array(
       '#type' => 'textfield',
-      '#maxlength' => 120,
+      '#maxlength' => 200,
       '#title' => t('Adresse web (URL)'),
       '#required' => TRUE,
       '#default_value' => $apidaeConfig->get('api.url')
@@ -76,7 +76,7 @@ class ApidaeSettingsForm extends ConfigFormBase {
     );
     $form['apidae_data']['selections'] = array(
       '#type' => 'textfield',
-      '#maxlength' => 120,
+      '#maxlength' => 200,
       '#title' => t('Sélections Apidae pré-calculées'),
       '#description' => t('Si vous avez plusieurs sélections, séparez-les par une virgule'),
       '#required' => FALSE,
