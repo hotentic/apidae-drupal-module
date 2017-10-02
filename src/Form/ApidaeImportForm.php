5971a9e01b10def056d1979993d9bea5462fb10f
Jean-Baptiste Vilain <jbvilain@gmail.com>
2017-10-02 11:35:55 +0200

    Added import button

---
+++ src/Form/ApidaeImportForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\config\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Defines the Apidae data import form.
+ */
+class ApidaeImportForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'apidae_import_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Importer les données Apidae'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRedirect('apidae.import');
+  }
+
+}
